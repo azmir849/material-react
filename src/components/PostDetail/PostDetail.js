035b868505696413f61b8f3f74a70246327204f1
azmir849 <azmir15-849@diu.edu.bd>
2020-09-05 16:37:49 +0600

    page route added

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 const PostDetail = () => {
+
+    const {id} = useParams();
+    const[post,setpost] = useState({});
+    useEffect(() =>{
+        const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setpost(data));
+
+    } , [])
+    const postStyle = {
+        border: '20px solid purple',
+        margin: '100px',
+        padding: '50px',
+        borderRadius :'50px'
+    }
+
     return (
-        <div>
-            <h4>Post Detail page</h4>
+        <div style={postStyle}>
+            <h4>Post ID : {post.id}</h4>
+            <h5>Post Title : {post.title}</h5>
+            <h6>Post Body : {post.body}</h6>
+            <h1>Post Comments : </h1>
         </div>
     );
 };
